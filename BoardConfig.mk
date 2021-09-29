5a8ff6e8b69a467ccefa55f766a193526ef75fde
chrisblue <wegtery9@outlook.com>
2021-09-29 18:51:49 +0800

    first commit

---
+++ BoardConfig.mk
@@ -0,0 +1,38 @@
+#
+# Copyright (C) 2018-2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from sdm845-common
+-include device/xiaomi/sdm845-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/xiaomi/polaris
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := polaris
+
+# Display
+TARGET_SCREEN_DENSITY := 440
+
+# Kernel
+TARGET_KERNEL_CONFIG := polaris_defconfig
+
+# HIDL
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
+
+# NFC
+TARGET_USES_NQ_NFC := true
+
+# Partitions
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
+
+# Properties
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+
+# Recovery
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
+
+# Inherit from the proprietary version
+-include vendor/xiaomi/polaris/BoardConfigVendor.mk
